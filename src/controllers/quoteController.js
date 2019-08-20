3963910d59c2e581828e0142aca499221d9f6a4e
fakhrad <fakhrad@openratio.com>
2019-08-20 09:18:57 +0430

    cancel and edit offer methods added to opportunity api

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -90,6 +90,49 @@ exports.issueOffer = function(req, res, next) {
     });
 };
 
+exports.editOffer = function(req, res, next) {
+  var accessToken = req.access_token;
+  var apiRoot =
+    process.env.SALESFORCE_API_ROOT ||
+    "https://crmdev-ponture-crmdev.cs84.force.com"; // for prod set to https://api.zignsec.com/v2
+  var config = {
+    url: "/services/apexrest/editOffer",
+    baseURL: apiRoot,
+    method: "put",
+    data: req.body,
+    headers: {
+      Authorization: "Bearer " + accessToken
+    }
+  };
+  console.log(config);
+  axios(config)
+    .then(function(response) {
+      if (response.data.success) res.send(response.data);
+      else res.status(response.data.statusCode).send(response.data);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        res.status(error.response.status).send(error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+        res.status(204).send("No response from server");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+        res.status(500).send(error.message);
+      }
+      console.log(error.config);
+      res.status(400).send(error.config);
+    });
+};
 exports.getoffers = function(req, res, next) {
   var accessToken = req.access_token;
   var apiRoot =
@@ -244,6 +287,50 @@ exports.fundedapplications = function(req, res, next) {
       partnerId: req.partnerId,
       stage: "offer won"
     },
+    headers: {
+      Authorization: "Bearer " + accessToken
+    }
+  };
+  console.log(config);
+  axios(config)
+    .then(function(response) {
+      if (response.data.success) res.send(response.data);
+      else res.status(response.data.statusCode).send(response.data);
+    })
+    .catch(function(error) {
+      if (error.response) {
+        // The request was made and the server responded with a status code
+        // that falls out of the range of 2xx
+        console.log(error.response.data);
+        console.log(error.response.status);
+        console.log(error.response.headers);
+        res.status(error.response.status).send(error.response.data);
+      } else if (error.request) {
+        // The request was made but no response was received
+        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+        // http.ClientRequest in node.js
+        console.log(error.request);
+        res.status(204).send("No response from server");
+      } else {
+        // Something happened in setting up the request that triggered an Error
+        console.log("Error", error.message);
+        res.status(500).send(error.message);
+      }
+      console.log(error.config);
+      res.status(400).send(error.config);
+    });
+};
+
+exports.cancelOffer = function(req, res, next) {
+  var accessToken = req.access_token;
+  var apiRoot =
+    process.env.SALESFORCE_API_ROOT ||
+    "https://crmdev-ponture-crmdev.cs84.force.com"; // for prod set to https://api.zignsec.com/v2
+  var config = {
+    url: "/services/apexrest/cancelOffer",
+    baseURL: apiRoot,
+    method: "get",
+    params: req.query,
     headers: {
       Authorization: "Bearer " + accessToken
     }
--- src/routes/quote.js
+++ src/routes/quote.js
@@ -1,13 +1,19 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var auth = require('../controllers/auth');
-var controller = require('../controllers/quoteController');
+var auth = require("../controllers/auth");
+var controller = require("../controllers/quoteController");
 router.get("/products", auth.verifyToken, controller.getPartnerProducts);
 router.post("/issue", auth.verifyToken, controller.issueOffer);
+router.post("/edit", auth.verifyToken, controller.editOffer);
 router.put("/accept", auth.verifyToken, controller.acceptOffer);
+router.put("/cancel", auth.verifyToken, controller.cancelOffer);
 router.get("/myoffers", auth.verifyToken, controller.getoffers);
 router.get("/acceptedoffers", auth.verifyToken, controller.acceptedoffers);
 router.get("/lostapplications", auth.verifyToken, controller.lostapplications);
-router.get("/fundedapplications", auth.verifyToken, controller.fundedapplications);
+router.get(
+  "/fundedapplications",
+  auth.verifyToken,
+  controller.fundedapplications
+);
 
 module.exports = router;
