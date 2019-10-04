bd953447fcaa60acd531c8b6c7cc5f7ae38e563d
fakhrad <fakhrad@openratio.com>
2019-10-04 17:36:12 +0330

    close reason api added to opportunity route

--- src/routes/opportunity.js
+++ src/routes/opportunity.js
@@ -1,7 +1,8 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
-var auth = require('../controllers/auth');
-var controller = require('../controllers/opportunityController');
+var auth = require("../controllers/auth");
+var controller = require("../controllers/opportunityController");
+router.get("/closereasons", auth.verifyToken, controller.getCloseReasons);
 router.get("/allnew", auth.verifyToken, controller.getNewApplications);
 router.get("/opened", auth.verifyToken, controller.getOpenedApplications);
 router.put("/open", auth.verifyToken, controller.openApplication);
