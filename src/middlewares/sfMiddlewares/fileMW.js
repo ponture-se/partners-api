0569b8ee8979ac989b7545434fd90b41462c1b93
Abbas <abbas.farkoosh@gmail.com>
2020-02-09 20:02:27 +0330

    "downloadFile" function added to middleware

---
+++ src/middlewares/sfMiddlewares/fileMW.js
@@ -0,0 +1,47 @@
+const fileController = require('../../controllers/fileController');
+const myResponse = require('../../controllers/myResponse');
+// const apiLogger = require('../apiLogger');
+// const logger = require('../../controllers/customeLogger');
+// const multer = require('multer');
+const dotenv = require('dotenv');
+// const fs = require('fs');
+dotenv.config();
+
+
+async function downloadFile(req, res, next) {
+    let resBody;
+    let sfConn = req.needs.sfConn;
+    let fileId = req.query.fileId;
+
+    let fileData = await fileController.getContentVersionWithFileId(fileId, sfConn);
+
+    if (fileData == null) {
+        resBody = myResponse(false, null, 500, 'Can not get file. Please recheck file Id and try again.');
+        res.status(500).send(resBody);
+        return next();
+    } else {
+        let fileName = Date.now().toString() + ' ' + fileData.title;
+
+        await fileController.downloadFileAsStream(fileId, fileName, sfConn, function (err) {
+            if (err) {
+                resBody = myResponse(false, null, 500, 'downloadFile Func Throw Error', err);
+                // logger.error('downloadFile Func Throw Error', {metadata: resBody});
+                console.log('downloadFile Func Throw Error', resBody);
+    
+                res.status(500).send(resBody);
+                return next();
+            } else {
+                let fileAddr = './tempStorage/' + fileName;
+                res.status(200).download(fileAddr);
+            }
+            
+        });
+    }
+
+}
+
+
+
+module.exports = {
+    downloadFile
+}
