5619e16b3d74bb4f177940864c8a398f774a10c7
Abbas <abbas.farkoosh@gmail.com>
2020-03-06 16:24:47 +0330

    fix "getCloseReasons" function

--- src/controllers/opportunityController.js
+++ src/controllers/opportunityController.js
@@ -13,55 +13,58 @@ exports.getCloseReasons = function (req, res, next) {
     var err = error_result;
     err.message = "PartnerID is invalid";
     res.status(400).send(err);
-  }
-  console.log(req.query);
-  var accessToken = req.access_token;
-  var apiRoot =
-    process.env.SALESFORCE_API_ROOT ||
-    "https://crmdev-ponture-crmdev.cs84.force.com"; // for prod set to https://api.zignsec.com/v2
-  var config = {
-    url: "/services/apexrest/spoCloseReason",
-    baseURL: apiRoot,
-    method: "get",
-    params: {
-      lang: "en"
-    },
-    headers: {
-      Authorization: "Bearer " + accessToken
-    }
-  };
-  console.log(config);
-  axios(config)
-    .then(function (response) {
-      if (response.data.success) res.send(response.data);
-      else
-        res
-        .status(response.data.statusCode ? response.data.statusCode : 200)
-        .send(response.data);
-    })
-    .catch(function (error) {
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        console.log(error.response.data);
-        console.log(error.response.status);
-        console.log(error.response.headers);
-        res.status(error.response.status).send(error.response.data);
-      } else if (error.request) {
-        // The request was made but no response was received
-        // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
-        // http.ClientRequest in node.js
-        console.log(error.request);
-        res.status(204).send("No response from server");
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        console.log("Error", error.message);
-        res.status(500).send(error.message);
+  } else {
+    console.log(req.query);
+    var accessToken = req.access_token;
+    var apiRoot =
+      process.env.SALESFORCE_API_ROOT ||
+      "https://crmdev-ponture-crmdev.cs84.force.com"; // for prod set to https://api.zignsec.com/v2
+    var config = {
+      url: "/services/apexrest/spoCloseReason",
+      baseURL: apiRoot,
+      method: "get",
+      params: {
+        lang: "en"
+      },
+      headers: {
+        Authorization: "Bearer " + accessToken
       }
-      console.log(error.config);
-      res.status(400).send(error.config);
-    });
+    };
+    console.log(config);
+    axios(config)
+      .then(function (response) {
+        if (response.data.success) res.send(response.data);
+        else
+          res
+          .status(response.data.statusCode ? response.data.statusCode : 200)
+          .send(response.data);
+      })
+      .catch(function (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+          res.status(error.response.status).send(error.response.data);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+          res.status(204).send("No response from server");
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log("Error", error.message);
+          res.status(500).send(error.message);
+        }
+        console.log(error.config);
+        res.status(400).send(error.config);
+      });
+  }
 };
+
+
 exports.getNewApplications = function (req, res, next) {
   if (!req.partnerId) {
     var err = error_result;
