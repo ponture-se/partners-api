02154e58d9c4c554b646b182f19e6cd64fadc4d0
Abbas <abbas.farkoosh@gmail.com>
2020-03-06 18:26:59 +0330

    fix "acceptOffer" function

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -452,6 +452,6 @@ exports.acceptOffer = function(req, res, next) {
         res.status(500).send(error.message);
       }
       console.log(error.config);
-      res.status(400).send(error.config);
+      // res.status(400).send(error.config);
     });
 };
