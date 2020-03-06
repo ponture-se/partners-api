f2482b0d851e1a53b19b361618d7e1386031e584
Abbas <abbas.farkoosh@gmail.com>
2020-03-06 15:03:02 +0330

    code cleaning

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 var auth = require('../controllers/auth');
+
+
 router.post("/login", auth.getSalesForceToken, auth.login);
+
 
 module.exports = router;
