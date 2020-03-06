3ba450745019ba5d2f985c4e178942a166638b33
Abbas <abbas.farkoosh@gmail.com>
2020-03-06 18:29:53 +0330

    fix "fundedapplications" function

--- src/controllers/quoteController.js
+++ src/controllers/quoteController.js
@@ -364,7 +364,7 @@ exports.fundedapplications = function(req, res, next) {
         res.status(500).send(error.message);
       }
       console.log(error.config);
-      res.status(400).send(error.config);
+      // res.status(400).send(error.config);
     });
 };
 
