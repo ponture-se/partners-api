f3246698d94a093ff8f98fc68c45c7b27b7599d5
Abbas <abbas.farkoosh@gmail.com>
2020-04-09 23:29:24 +0430

    "acceptedOfferCanceledController" revised

--- src/controllers/triggerController.js
+++ src/controllers/triggerController.js
@@ -1,6 +1,7 @@
 // const accCtrl = require('./accountController');
 const logger = require('./customeLogger');
 const productCtrl = require('./productController');
+const emailCtrl = require('./emailController');
 // const myToolkit = require('./myToolkit');
 // const feedCtrl = require('./feedItemController');
 const _ = require('lodash');
@@ -8,13 +9,13 @@ const _ = require('lodash');
 async function acceptedOfferCanceledController(sfConn, oppId) {
     // Section: Get Offers with details
     const proActiveStage = [
-        // 'Offer Issued',
-        // 'Offer Accepted'
+        'Offer Issued',
+        'Offer Accepted'
     ]
 
     const whereClause = {
-        // "Supplier_Partner_Opportunity__r.OpportunityId__c": oppId,
-        // stage__c: {$in: proActiveStage}
+        "Supplier_Partner_Opportunity__r.OpportunityId__c": oppId,
+        stage__c: {$in: proActiveStage}
     }
 
     let productsObject = await productCtrl.getProdcutsOfAllPartnersWithDetailsWhere(sfConn, whereClause);
@@ -22,17 +23,26 @@ async function acceptedOfferCanceledController(sfConn, oppId) {
     let partnerPMasterMap = productsObject.partnerPMasterMap;
     let trBoxPerCobjName = productsObject.trBoxPerCobjName;
 
-    let showInList = perPartnerShowInList(partnerPMasterMap, trBoxPerCobjName);
-
+    
     // Section: Send Mail if any product exist
+    console.log("Products List", productsList);
+    if (productsList.length > 0) {
+        let perPartnerShowInList = generatePerPartnerShowInList(partnerPMasterMap, trBoxPerCobjName);
+        let emailsList = emailCtrl.prepareEmailForProducts(productsList, perPartnerShowInList, 1);
 
+        // try{
+        emailCtrl.callSfSendMailAPI(sfConn, emailsList);
+        // } catch (e) {
+        //     console.log(e);
+        // }
+    }
 
-    return productList;
+    return null;
+    // return productList;
 }
 
 
-
-function perPartnerShowInList(partnerPMasterMap, trBoxPerCobjName) {
+function generatePerPartnerShowInList(partnerPMasterMap, trBoxPerCobjName) {
     let result = {};
 
     for (let partnerId in partnerPMasterMap) {
@@ -40,9 +50,14 @@ function perPartnerShowInList(partnerPMasterMap, trBoxPerCobjName) {
         let trOfPartner = trBoxPerCobjName[customObjectName];
 
         if (trOfPartner.success) {
-            let productTypesOfPartner = _.groupBy(_.filter(trOfPartner.result, o => {
+            // Note: Now, just consider Loan Type for Products
+            let productTypesOfPartner = _.filter(trOfPartner.result, o => {
                 return o.Show_in_List__c;
-            }), 'Product_Type__c');
+            });
+
+            // let productTypesOfPartner = _.groupBy(_.filter(trOfPartner.result, o => {
+            //     return o.Show_in_List__c;
+            // }), 'Product_Type__c');
 
             result[partnerId] = productTypesOfPartner;
         }
@@ -50,6 +65,7 @@ function perPartnerShowInList(partnerPMasterMap, trBoxPerCobjName) {
 
     return result;
 }
+
 
 module.exports = {
     acceptedOfferCanceledController
