711966a1c0107f828bc665fb27dab8b8df642437
Abbas <abbas.farkoosh@gmail.com>
2020-04-12 15:52:35 +0430

    revise fundApp model

--- src/models/qouteModel.js
+++ src/models/qouteModel.js
@@ -8,11 +8,13 @@ function fundAppValidation() {
             .trim()
             .not().isEmpty().withMessage("Can not be Empty"),
         query('loanAmount').isFloat({gt: 0.0}).withMessage('It Should be Float Greater Than 0.0')
-            .exists().withMessage("Required Key/Value Pair")
+            .optional()
+            // .exists().withMessage("Required Key/Value Pair")
             .trim()
             .not().isEmpty().withMessage("Can not be Empty"),
         query('loanPeriod').isInt({gt: 0}).withMessage('It Should be Integer Number Greater Than 0')
-            .exists().withMessage("Required Key/Value Pair")
+            // .exists().withMessage("Required Key/Value Pair")
+            .optional()
             .trim()
             .not().isEmpty().withMessage("Can not be Empty")
 	];
