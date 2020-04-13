791c00411132ece98021712f83a719acd54d21d0
Abbas <abbas.farkoosh@gmail.com>
2020-04-13 17:12:41 +0430

    "prepareEmailForTrigger2" function added

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,6 +1,28 @@
 const _ = require('lodash');
 const logger = require('./customeLogger');
 
+function prepareEmailForTrigger2(productList) {
+    // FIXME: Mock MAIL
+    let emailsList = [];
+
+    let subject = 'Partner Info By Trigger 2';
+
+    productList.forEach(pro => {
+        let primaryContact = _.get(pro, 'Supplier_Partner_Opportunity__r.OpportunityId__r.PrimaryContact__r');
+
+        let contactEmail = (primaryContact) ? _.get(primaryContact, 'Email') : null;
+        let whatId = _.get(pro, 'Supplier_Partner_Opportunity__r.OpportunityId__c', null);
+
+        let body = JSON.stringify(_.get(pro, 'Supplier_Partner_Opportunity__r.SupplierAccountId__r', 'NO INFORMATION FOUND'), null, 2);
+
+        if (contactEmail) {
+            emailsList.push(createMailObject(contactEmail, subject, body, whatId));
+        }
+    });
+
+    return emailsList;
+}
+
 
 function createMailObject(to, subject, body, whatId = null) {
 
@@ -15,7 +37,6 @@ function createMailObject(to, subject, body, whatId = null) {
 
 
 async function callSfSendMailAPI(sfConn, emailsList) {
-    console.log("emailsList", emailsList);
     let reqBody = {
         emailsList : emailsList
     }
@@ -27,5 +48,6 @@ async function callSfSendMailAPI(sfConn, emailsList) {
 }
 
 module.exports = {
-    callSfSendMailAPI
+    callSfSendMailAPI,
+    prepareEmailForTrigger2
 }
