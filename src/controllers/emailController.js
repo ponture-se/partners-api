8018a794a6dbf1774e350538ea77931491390985
Abbas <abbas.farkoosh@gmail.com>
2020-04-13 18:51:33 +0430

    "prepareEmailForTrigger7" function added (mock)

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,6 +1,29 @@
 const _ = require('lodash');
 const logger = require('./customeLogger');
 
+// Trigger 7
+function prepareEmailForTrigger7(productList, perPartnerShowInList) {
+    // FIXME: Mock Mail + Use perPartnerShowInList
+    let emailsList = [];
+
+    let subject = 'Partner Info By Trigger 7';
+
+    productList.forEach(pro => {
+        let primaryContact = _.get(pro, 'Supplier_Partner_Opportunity__r.OpportunityId__r.PrimaryContact__r');
+
+        let contactEmail = (primaryContact) ? _.get(primaryContact, 'Email') : null;
+        let whatId = _.get(pro, 'Supplier_Partner_Opportunity__r.OpportunityId__c', null);
+
+        let body = JSON.stringify(pro, null, 2);
+
+        if (contactEmail) {
+            emailsList.push(createMailObject(contactEmail, subject, body, whatId));
+        }
+    });
+
+    return emailsList;
+}
+
 
 function createMailObject(to, subject, body, whatId = null) {
 
@@ -27,5 +50,6 @@ async function callSfSendMailAPI(sfConn, emailsList) {
 }
 
 module.exports = {
-    callSfSendMailAPI
+    callSfSendMailAPI,
+    prepareEmailForTrigger7
 }
