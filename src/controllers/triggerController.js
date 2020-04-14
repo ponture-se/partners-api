d6375fa189db1d1d608fa20ec1620eeb38222e27
Abbas <abbas.farkoosh@gmail.com>
2020-04-14 18:41:41 +0430

    "sendActiveOffersToCustomerController_case6" function added

--- src/controllers/triggerController.js
+++ src/controllers/triggerController.js
@@ -287,6 +287,106 @@ async function sendActiveOffersToCustomerController_case5(sfConn) {
 
 }
 
+
+async function sendActiveOffersToCustomerController_case6(sfConn) {
+    // Note: Trigger 6- send an email to the customer if customer has applied between 00:00 to 11:59:PM PM three days before and has at least one active offers and no accepted offer(the day before the day before yesterday)
+
+    let offerDeactiveStages = [
+        'Canceled',
+        'Offer Lost',
+        'Offer Won'
+    ];
+
+    // caseNumber 6
+    // Section: get opportunity with stage: offer recieved in time
+    let oppWhere = {
+        stageName: 'Offer Received',
+        lastModifiedDate: jsforce.Date.LAST_N_DAYS(3)
+    }
+    let hourCondition = {
+        min: 0,
+        max: 12
+    }
+    let oppList = await sfConn.sobject('opportunity')
+                            .select('*')
+                            .where(oppWhere)
+                            .execute();
+    
+    
+    let oppListId = _.map(oppList, 'Id');
+    // Section: check feedItem
+    let feedList = await feedCtrl.getFeedTrackItems(sfConn, {
+                                    $and: [
+                                        {
+                                            LastModifiedDate: {
+                                                $eq: jsforce.Date.LAST_N_DAYS(3)
+                                            }
+                                        },
+                                        {
+                                            LastModifiedDate: {
+                                                $ne: jsforce.Date.LAST_N_DAYS(2)
+                                            }
+                                        }
+                                    ],
+                                    type: 'TrackedChange',
+                                    parentId: {$in: oppListId}
+                                });
+    let filteredFeeds = feedCtrl.filterSpecificTrackChangeFeeds(feedList, 'stageName', null, 'Offer Received');
+    let filteredFeedsByHour = _.filter(filteredFeeds, o => {
+                                        let lastModifiedDate = o.LastModifiedDate;
+                                        let localeLastModifiedDate = myToolkit.convertDatetimeToSwedenLocale(lastModifiedDate) ;
+                                        let localHour = myToolkit.getHourOfDateString(localeLastModifiedDate);
+
+                                        if (localHour >= hourCondition.min && localHour < hourCondition.max) {
+                                            return true;
+                                        }
+                                    });
+    
+    let finalOppListId = _.map(filteredFeedsByHour, 'ParentId');
+
+    // Section: get active offers of these opps
+    // if (filteredOppListByHourId.length == 0) return;
+    if (finalOppListId.length == 0) return;
+    let productWhere = {
+        "Supplier_Partner_Opportunity__r.OpportunityId__c" : {$in: finalOppListId},
+        // "Supplier_Partner_Opportunity__r.OpportunityId__c" : {
+        //     $in: filteredOppListByHourId
+        // },
+        $and: [
+                {stage__c: {$nin: offerDeactiveStages}},
+                {stage__c: {$ne: 'Offer Accepted'}}
+            ]
+    }
+
+    let offersList = await productCtrl.getProductsWhere(sfConn, productWhere);
+
+    if (offersList.length == 0) return;
+
+    // Section: get product details by id
+    // Get Desired Partner Ids to get Product Details
+    let desiredPartnerId = _.compact(_.map(offersList, o => {
+        return _.get(o, 'Supplier_Partner_Opportunity__r.SupplierAccountId__c', null);
+    }));
+    let partnerWhere = {
+        id: {$in: desiredPartnerId}
+    }
+
+    let productsObject = await productCtrl.getProdcutsWithDetailsWhere_specificPartner(sfConn, partnerWhere, productWhere);
+    let productsList = productsObject.productsList;
+    let partnerPMasterMap = productsObject.partnerPMasterMap;
+    let trBoxPerCobjName = productsObject.trBoxPerCobjName;
+
+    // Section: email Section
+    if (productsList.length > 0) {
+        let perPartnerShowInList = generatePerPartnerShowInList(partnerPMasterMap, trBoxPerCobjName);
+        let emailsList = emailCtrl.prepareEmailForTriggerActiveOffers(productsList, perPartnerShowInList);
+        
+        emailCtrl.callSfSendMailAPI(sfConn, emailsList);
+        
+    }
+
+}
+
 function generatePerPartnerShowInList(partnerPMasterMap, trBoxPerCobjName) {
     let result = {};
 
@@ -317,5 +417,6 @@ function generatePerPartnerShowInList(partnerPMasterMap, trBoxPerCobjName) {
 module.exports = {
     sendActiveOffersToCustomerController_case3,
     sendActiveOffersToCustomerController_case4,
-    sendActiveOffersToCustomerController_case5
+    sendActiveOffersToCustomerController_case5,
+    sendActiveOffersToCustomerController_case6
 }
