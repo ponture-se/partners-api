2d74f1bc0e0173bbe6a7ecb0cb93dc02563d9252
Abbas <abbas.farkoosh@gmail.com>
2020-04-14 18:03:31 +0430

    "/sendActiveOffersToCustomer" route added

--- src/routes/trigger.js
+++ src/routes/trigger.js
@@ -3,8 +3,11 @@ var router = express.Router();
 var triggerMW = require("../middlewares/triggerMW");
 const getSFConnection = require("../middlewares/sfMiddleware");
 
-
-
+// Trigger #3,4,5,6
+router.post('/sendActiveOffersToCustomer',
+            // todo: validation,
+            getSFConnection,
+            triggerMW.sendActiveOffersToCustomerApi);
 
 
 
