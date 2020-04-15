edb70683cebb7ca6e804945abe1a049b890b04ed
Abbas <abbas.farkoosh@gmail.com>
2020-04-15 14:28:52 +0430

    "sendOverviewToPartners_EmailTriggerApi" function added

--- src/middlewares/triggerMW.js
+++ src/middlewares/triggerMW.js
@@ -4,10 +4,25 @@ const triggerCtrl = require('../controllers/triggerController');
 const emailCtrl = require('../controllers/emailController');
 const { salesforceException } = require('../controllers/customeException');
 
+async function sendOverviewToPartners_EmailTriggerApi(req, res, next) {
+    let sfConn = req.needs.sfConn;
 
+    let resBody;
+    try{
+        await triggerCtrl.sendOverviewToPartners_EmailTriggerController(sfConn);
+        resBody = myResponse(true, null, 200, 'Emails are Sent to SF API.');
+        res.status(200).send(resBody);
+    } catch (e) {
+        resBody = myResponse(false, null, 500, 'Something Went Wrong.', e);
+        res.status(500).send(resBody);
+    }
+
+    return next();
+}
 
 
 module.exports = {
+    sendOverviewToPartners_EmailTriggerApi
     
 }
 
