063d191d229a21e053c8561edff09b356720a201
Abbas <abbas.farkoosh@gmail.com>
2020-04-15 14:28:17 +0430

    "prepareOverviewEmailForPartners" function added

--- src/controllers/emailController.js
+++ src/controllers/emailController.js
@@ -1,6 +1,42 @@
 const _ = require('lodash');
 const logger = require('./customeLogger');
 
+function prepareOverviewEmailForPartners(partners, productListPerPartners, spoListPerPartners) {
+    // FIXME: Mock Email
+    let emailsList = [];
+
+    let subject = 'Check The Overview of Your Ponture Account'
+
+    for (let [partnerId, partnerList] of Object.entries(partners)) {
+        let partner = partnerList[0];
+        let partnerEmail = _.get(partner, 'Email__c');
+        let productOfPartner = _.get(productListPerPartners, partnerId);
+        let spoOfPartner = _.get(spoListPerPartners, partnerId);
+
+        let whatId = partnerId;
+
+        let body = '<div>' + partner.Name + ': </div>' +
+                    '<hr>'+
+                    '<br/>' +
+                    '<br/>' +
+                    '<div>Your Accepted Products</div>' +
+                    '<br/>' +
+                    '<div>' + JSON.stringify(productOfPartner, null, 2) + ': </div>' +
+                    '<br/>' +
+                    '<br/>' +
+                    '<hr>'+
+                    '<div>Your SPO</div>' +
+                    '<br/>' +
+                    '<div>' + JSON.stringify(spoOfPartner, null, 2) + ': </div>';
+
+        if (partnerEmail) {
+            emailsList.push(createMailObject(partnerEmail, subject, body, whatId));
+        }
+    }
+
+    return emailsList;
+}
+
 
 function createMailObject(to, subject, body, whatId = null) {
 
@@ -27,5 +63,6 @@ async function callSfSendMailAPI(sfConn, emailsList) {
 }
 
 module.exports = {
-    callSfSendMailAPI
+    callSfSendMailAPI,
+    prepareOverviewEmailForPartners
 }
