3f7c8c8dd25bb68e8c27718ba24a63a9f0c89cb3
Abbas <abbas.farkoosh@gmail.com>
2020-04-21 16:48:30 +0430

    add trigger model and validation

--- src/routes/trigger.js
+++ src/routes/trigger.js
@@ -2,37 +2,40 @@ var express = require("express");
 var router = express.Router();
 var triggerMW = require("../middlewares/triggerMW");
 const getSFConnection = require("../middlewares/sfMiddleware");
+const validate = require("../middlewares/validate");
+const triggerValidationRules = require("../models/triggerModel");
 
 
 // Trigger #1
 router.post('/acceptedOfferCanceled',
-                // todo: validation (check offer to be in list)
+                triggerValidationRules.acceptedOfferCanceledModel(),
+                validate,
                 getSFConnection,
                 triggerMW.acceptedOfferCanceledApi
                 );
 
 // Trigger 2
 router.post("/sendYesterdayAcceptedPartnerInfo",
-            // todo: add validation
             getSFConnection,
             triggerMW.sendYesterdayAcceptedPartnerInfoApi
             );
 
 // Trigger #3,4,5,6
 router.post('/sendActiveOffersToCustomer',
-            // todo: validation,
+            triggerValidationRules.sendActiveOffersToCustomerModel(),
+            validate,
             getSFConnection,
             triggerMW.sendActiveOffersToCustomerApi);
 
 // Trigger #7
 router.post('/realTimeEmailAfterAcceptance', 
-            // todo: validation
+            triggerValidationRules.realTimeEmailAfterAcceptanceModel(),
+            validate,
             getSFConnection,
             triggerMW.realTimeEmailAfterAcceptanceApi);
 
 // Trigger #8
 router.post('/sendOverviewToPartners', 
-            // todo: validation
             getSFConnection,
             triggerMW.sendOverviewToPartners_EmailTriggerApi);
 
